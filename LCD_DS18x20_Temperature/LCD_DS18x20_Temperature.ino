7f6d31ecc640dc467a7d27347736b33a6753a4b9
Mariusz Nowicki <marnowicki@wp.pl>
2020-11-11 21:31:06 +0100

    updated code with DS18B20 readmings

--- LCD_DS18x20_Temperature/LCD_DS18x20_Temperature.ino
+++ LCD_DS18x20_Temperature/LCD_DS18x20_Temperature.ino
@@ -1,21 +1,25 @@
 #include <OneWire.h>
-#include <LiquidCrystal.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+static char str[15];
+boolean badReading = true;
+unsigned long time;
 
 // OneWire DS18S20, DS18B20, DS1822 Temperature Example
 //
 // http://www.pjrc.com/teensy/td_libs_OneWire.html
 //
 // The DallasTemperature library can do all this work for you!
-// http://milesburton.com/Dallas_Temperature_Control_Library
+// https://github.com/milesburton/Arduino-Temperature-Control-Library
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 OneWire  ds(10);  // on pin 10 (a 4.7K resistor is necessary)
 
 void setup(void) {
+   lcd.init();                      // initialize the lcd 
+  lcd.backlight();
   Serial.begin(9600);
-    lcd.begin(16,2);
-    lcd.setCursor(0,0);
-    lcd.print("DS1820 Test");
 }
 
 void loop(void) {
@@ -25,68 +29,70 @@ void loop(void) {
   byte data[12];
   byte addr[8];
   float celsius, fahrenheit;
+
+  //time = millis();
   
   if ( !ds.search(addr)) {
     Serial.println("No more addresses.");
     Serial.println();
     ds.reset_search();
-    delay(250);
+    badReading ? delay(100) : delay(2000);
     return;
   }
   
-  Serial.print("ROM =");
-  for( i = 0; i < 8; i++) {
-    Serial.write(' ');
-    Serial.print(addr[i], HEX);
-  }
+//  Serial.print("ROM =");
+//  for( i = 0; i < 8; i++) {
+//    Serial.write(' ');
+//    Serial.print(addr[i], HEX);
+//  }
 
   if (OneWire::crc8(addr, 7) != addr[7]) {
       Serial.println("CRC is not valid!");
       return;
   }
-  Serial.println();
+//  Serial.println();
  
   // the first ROM byte indicates which chip
   switch (addr[0]) {
     case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
+      //Serial.println("  Chip = DS18S20");  // or old DS1820
       type_s = 1;
       break;
     case 0x28:
-      Serial.println("  Chip = DS18B20");
+      //Serial.println("  Chip = DS18B20");
       type_s = 0;
       break;
     case 0x22:
-      Serial.println("  Chip = DS1822");
+      //Serial.println("  Chip = DS1822");
       type_s = 0;
       break;
     default:
-      Serial.println("Device is not a DS18x20 family device.");
+      //Serial.println("Device is not a DS18x20 family device.");
       return;
   } 
 
   ds.reset();
   ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-  
-  delay(1000);     // maybe 750ms is enough, maybe not
+ // ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  ds.write(0x44, 0);
+  //delay(1000);     // maybe 750ms is enough, maybe not
   // we might do a ds.depower() here, but the reset will take care of it.
   
   present = ds.reset();
   ds.select(addr);    
   ds.write(0xBE);         // Read Scratchpad
 
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
+//  Serial.print("  Data = ");
+//  Serial.print(present, HEX);
+//  Serial.print(" ");
   for ( i = 0; i < 9; i++) {           // we need 9 bytes
     data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
+//    Serial.print(data[i], HEX);
+//    Serial.print(" ");
   }
-  Serial.print(" CRC=");
-  Serial.print(OneWire::crc8(data, 8), HEX);
-  Serial.println();
+//  Serial.print(" CRC=");
+//  Serial.print(OneWire::crc8(data, 8), HEX);
+//  Serial.println();
 
   // Convert the data to actual temperature
   // because the result is a 16 bit signed integer, it should
@@ -108,12 +114,20 @@ void loop(void) {
     //// default is 12 bit resolution, 750 ms conversion time
   }
   celsius = (float)raw / 16.0;
+  badReading = celsius > 84;
+
   fahrenheit = celsius * 1.8 + 32.0;
+  dtostrf(celsius, 4,1, str);
   Serial.print("  Temperature = ");
   Serial.print(celsius);
-  lcd.setCursor(0,1);
-  lcd.print(celsius);
-  Serial.print(" Celsius, ");
-  Serial.print(fahrenheit);
-  Serial.println(" Fahrenheit");
+  Serial.println(" C, ");
+
+  if(!badReading){
+    lcd.clear();
+    lcd.print("Temp = ");
+    lcd.print(str);
+    lcd.print(" C ");
+  }
+  //Serial.print(fahrenheit);
+  //Serial.println(" Fahrenheit");
 }
